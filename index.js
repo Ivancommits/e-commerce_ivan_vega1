83daa93bd75c2c126d968a9f527ea17f00c2fac3
Edu <edu@gmail.com>
2024-09-16 20:42:25 -0300

    sprint 1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(name);
+let h1 = document.querySelector("h1").innerText = "Productos";
+
+let array = [];
+
+for (let i = 1; i < 10; i++) {
+    const card = `
+   <div class="card" style="width: 18rem;">
+  <img src="https://http2.mlstatic.com/D_NQ_NP_875670-MLA54151274878_032023-O.webp" class="card-img-top" alt="Guitarra">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+array.push(array);
+} 
+document.querySelector('.container').innerHTML = prod.join("");
