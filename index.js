ad2e6a25e33ddef7da5d85a0f720c85ec4007b00
Edu <edu@gmail.com>
2024-09-19 20:02:00 -0300

    Correcciones

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ let h1 = document.querySelector("h1").innerText = "Productos";
 let array = [];
 
 for (let i = 1; i < 10; i++) {
-    const card = `
-   <div class="card" style="width: 18rem;">
-  <img src="https://http2.mlstatic.com/D_NQ_NP_875670-MLA54151274878_032023-O.webp" class="card-img-top" alt="Guitarra">
+    const card = `<div class="card" style="width: 18rem;">
+  <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="imagen ${i}" style="object-fit: cover; width: 100%; height: 150px;"/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>`;
-array.push(array);
+array.push(card);
 } 
-document.querySelector('.container').innerHTML = prod.join("");
+document.querySelector('.container').innerHTML = array.join("");
